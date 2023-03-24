c3ffe28121dd24acf1ab2e74d115c4f5758876a8
Ghost <game2004g2@gmail.com>
2023-03-24 23:11:02 +0300

    Add request modal features

--- DMC_Bot/UI/test_modal.py
+++ DMC_Bot/UI/test_modal.py
@@ -1,11 +1,17 @@
 import discord
 from discord.ui import Modal, View, InputText, Button, Item
-from discord import Embed, Interaction, InputTextStyle, ApplicationContext, Emoji
+from discord import Embed, Interaction, InputTextStyle
 
 
 class RequestModal(Modal):
     def __init__(self, title: str = "Заявка", *args, **kwargs) -> None:
         super().__init__(title=title, *args, **kwargs)
+
+        # Config
+        self.ephemeral_message = "Заявка успешно отправлена"
+        self.channel_id = 784984599435804672
+        self.up_emoji = "<:up_:1088899600997417071>"
+        self.down_emoji = "<:down:1088899619632730184>"
 
         self.add_item(InputText(label="Ваш ник в Minecraft", placeholder="Введите ник в Minecraft"))
         self.add_item(InputText(label="Сколько вам лет?", placeholder="Введите ваш возраст"))
@@ -19,10 +25,11 @@ class RequestModal(Modal):
         embed.set_author(name=interaction.user.name)
         embed.set_thumbnail(url=interaction.user.avatar)
 
-        await interaction.response.send_message(embeds=[embed])
-        msg: discord.InteractionMessage = await interaction.original_response()
-        await msg.add_reaction("<:up_:1088899600997417071>")
-        await msg.add_reaction("<:down:1088899619632730184>")
+        await interaction.response.send_message(content=self.ephemeral_message, ephemeral=True)
+        channel = interaction.guild.get_channel(self.channel_id)
+        msg = await channel.send(embed=embed)
+        await msg.add_reaction(self.up_emoji)
+        await msg.add_reaction(self.down_emoji)
 
 
 class RequestView(View):
